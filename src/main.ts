4fddffb0bb17739743326241285d794618759f02
tbvjaos510 <tbvjaos510@naver.com>
2020-10-30 00:02:08 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { getInput, setOutput, setFailed } from "@actions/core";
-import github from "@actions/github";
+import * as github from "@actions/github";
 
 async function run(): Promise<void> {
 	try {
